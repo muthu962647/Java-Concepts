e12125546e99ea92096a57d239a7384381886669
Muthukumar Balasubramanian <129060533+muthu962647@users.noreply.github.com>
2024-09-04 18:17:49 +0530

    Update FunctionalInterface.java

--- FunctionalInterface.java
+++ FunctionalInterface.java
@@ -3,9 +3,24 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+
+@FunctionalInterface
+interface Sayit<T>{
+    default void greet(){
+        System.out.println("Greetings!!!...");
+    }
+    void saying(T t);
+}
+
+
 public class Main
 {
 	public static void main(String[] args) {
+
+
+	Sayit<String> sayit = (name) -> System.out.println("You look wonderful today.. "+ name);
+        sayit.saying("Muthu");
+	
 
 //Implementing Predicate Functional Interface
 	Predicate<Integer> predicate = (value) -> true;
@@ -26,4 +41,5 @@ public class Main
 //Implementing Supplier Function Interface
         Supplier<String> supplier = () ->  "name";
         System.out.println(supplier.get());
+
 }
