db4522f204909edac1f805777de4b00b5866a212
Muthukumar Balasubramanian <129060533+muthu962647@users.noreply.github.com>
2024-09-04 18:02:50 +0530

    Update and rename Functional Interface to FunctionalInterface.java

---
+++ FunctionalInterface.java
@@ -0,0 +1,28 @@
+import java.util.function.Predicate;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Main
+{
+	public static void main(String[] args) {
+
+//Implementing Predicate Functional Interface
+	Predicate<Integer> predicate = (value) -> true;
+        System.out.println(predicate.test(1)); 
+
+//Implementing Consumer Functional Interface
+        Consumer<String> consumer = (String name) -> System.out.println("Hello "+ name);
+        consumer.accept("Muthukumar");
+	}
+
+//Implementing Function Functional Interface
+        Function<String, Integer> function = (name) -> {
+            int age = 23;
+            return age;
+        };
+
+        function.apply("Muthukumar");
+//Implementing Supplier Function Interface
+        Supplier<String> supplier = () ->  "name";
+        System.out.println(supplier.get());
+}
