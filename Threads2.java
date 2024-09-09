2703479408bdd8b8953de2e75b98d1313074e909
Muthukumar Balasubramanian <129060533+muthu962647@users.noreply.github.com>
2024-09-09 12:52:07 +0530

    Update Threads2.java

--- Threads2.java
+++ Threads2.java
@@ -1,15 +1,27 @@
 package org.example;
 
 
+class Table{
+     synchronized public void Multiply(int n){
+        for(int i = 0; i < 10; i++){
+            System.out.println(i*n);
+        }
+    }
+}
+
 public class Main {
+    Table t = new Table();
     public static void main(String[] args) {
         Runnable a = () -> {
+
+            t.Multiply(7);
             for(int i = 0; i < 6; i++){
                 System.out.println("Hi");
                 try {Thread.sleep(1000);} catch (InterruptedException e) {throw new RuntimeException(e);}
             }
         };
         Runnable b = () -> {
+            t.Multiply(10);
             for(int i = 0; i < 6; i++){
                 System.out.println("Hello");
                 try {Thread.sleep(1000);} catch (InterruptedException e) {throw new RuntimeException(e);}
